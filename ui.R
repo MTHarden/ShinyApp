61078be57892ab70929fcc08204f3b4854c81ccb
Mitch Harden <mitch.harden@gmail.com>
2014-11-25 13:55:52 -0600

    Updated for clarity

--- ui.R
+++ ui.R
@@ -3,14 +3,14 @@ shinyUI(
                 # Application title
                 headerPanel("Are you a Cradle Robber?"),
                 sidebarPanel(
-                        numericInput('YourAge', 'Your Age in Years', 18, min = 14, max = 100, step = 1),
+                        numericInput('YourAge', 'Your Age in Years (Min=14)', 18, min = 14, max = 100, step = 1),
                         submitButton('Submit')
                 ),
                 mainPanel(
                         h3('Results of prediction'),
                         h4('You entered for your age'),
                         verbatimTextOutput("inputValue"),
-                        h4('The youngest age your partner can acceptably be '),
+                        h4('The youngest age your romantic partner can acceptably be '),
                         verbatimTextOutput("prediction")
                 )
         )
